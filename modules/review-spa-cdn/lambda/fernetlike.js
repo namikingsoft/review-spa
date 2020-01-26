4124ab5754b618838083b85dcb93da94f7e1180d
Tsubasa Namiki <namikingsoft@gmail.com>
2020-01-26 17:16:10 +0900

    style: change js indent to 2
    PEP8 に合わせない

--- modules/review-spa-cdn/lambda/fernetlike.js
+++ modules/review-spa-cdn/lambda/fernetlike.js
@@ -5,46 +5,46 @@ const separator = '-';
 const cryptAlgorism = 'AES-128-CBC';
 
 const encrypt = (payload, key) => {
-    const cipher = crypto.createCipher(cryptAlgorism, key);
-    return cipher.update(payload, 'utf-8', 'base64') + cipher.final('base64');
+  const cipher = crypto.createCipher(cryptAlgorism, key);
+  return cipher.update(payload, 'utf-8', 'base64') + cipher.final('base64');
 };
 
 const decrypt = (crypted, key) => {
-    const decipher = crypto.createDecipher(cryptAlgorism, key);
-    return decipher.update(crypted, 'base64', 'utf-8') + decipher.final('utf-8');
+  const decipher = crypto.createDecipher(cryptAlgorism, key);
+  return decipher.update(crypted, 'base64', 'utf-8') + decipher.final('utf-8');
 };
 
 const createHKDF = (key, salt) => {
-    const secureKey = crypto.createHmac('sha256', key)
-        .update(salt)
-        .digest('base64')
-        .toString();
-    return payload => crypto
-        .createHmac('sha256', secureKey)
-        .update(payload)
-        .digest('base64')
-        .toString();
+  const secureKey = crypto.createHmac('sha256', key)
+    .update(salt)
+    .digest('base64')
+    .toString();
+  return payload => crypto
+    .createHmac('sha256', secureKey)
+    .update(payload)
+    .digest('base64')
+    .toString();
 }
 
 const createFernetLike = ({ signerKey, cryptoKey, salt }) => {
-    const hkdf = createHKDF(signerKey, salt);
-    return {
-        sign(payload, maxAgeSec) {
-            const crypted = encrypt(payload, cryptoKey);
-            const expired = String(Math.floor(new Date().getTime() / 1000) + maxAgeSec);
-            const signature = hkdf(expired + crypted);
-            return [header, expired, crypted, signature].join(separator);
-        },
-        verify(token) {
-            if (!token.startsWith(header)) throw new Error('Token not found');
-            const [, expired, crypted, signature] = token.split(separator);
-            if (Number(expired) * 1000 < new Date().getTime()) throw new Error('Expired token');
-            if (signature === hkdf(expired + crypted)) {
-                return decrypt(crypted, cryptoKey);
-            }
-            throw new Error('Verification failure');
-        },
-    };
+  const hkdf = createHKDF(signerKey, salt);
+  return {
+    sign(payload, maxAgeSec) {
+      const crypted = encrypt(payload, cryptoKey);
+      const expired = String(Math.floor(new Date().getTime() / 1000) + maxAgeSec);
+      const signature = hkdf(expired + crypted);
+      return [header, expired, crypted, signature].join(separator);
+    },
+    verify(token) {
+      if (!token.startsWith(header)) throw new Error('Token not found');
+      const [, expired, crypted, signature] = token.split(separator);
+      if (Number(expired) * 1000 < new Date().getTime()) throw new Error('Expired token');
+      if (signature === hkdf(expired + crypted)) {
+        return decrypt(crypted, cryptoKey);
+      }
+      throw new Error('Verification failure');
+    },
+  };
 };
 
 module.exports = createFernetLike;
--- modules/review-spa-cdn/lambda/jsonRequest.js
+++ modules/review-spa-cdn/lambda/jsonRequest.js
@@ -1,26 +1,26 @@
 const https = require("https");
 
 const jsonRequest = ({ url, body, method = 'GET', headers: overwriteHeaders }) => new Promise((resolve, reject) => {
-    const headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Content-Length': body ? body.length : 0,
-        ...overwriteHeaders,
-    };
-    console.log('request:', JSON.stringify({ method, url, body, headers }));
-    var req = https.request(url, { method, headers }, res => {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
-        let xBody = '';
-        res.on('data', x => xBody += x);
-        res.on('end', () => {
-            console.log('body', xBody);
-            resolve(xBody);
-        });
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Content-Length': body ? body.length : 0,
+    ...overwriteHeaders,
+  };
+  console.log('request:', JSON.stringify({ method, url, body, headers }));
+  var req = https.request(url, { method, headers }, res => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+    let xBody = '';
+    res.on('data', x => xBody += x);
+    res.on('end', () => {
+      console.log('body', xBody);
+      resolve(xBody);
     });
-    if (body) req.write(body);
-    req.on('error', reject);
-    req.end();
+  });
+  if (body) req.write(body);
+  req.on('error', reject);
+  req.end();
 });
 
 module.exports = jsonRequest;
