498b2c056dd8ae41aef6c68cc842b50480bb7d6b
Tsubasa Namiki <namikingsoft@gmail.com>
2020-02-03 22:21:50 +0900

    fix: more close to fernet

--- modules/review-spa-cdn/lambda/fernetLike.js
+++ modules/review-spa-cdn/lambda/fernetLike.js
@@ -1,48 +1,52 @@
+// Fernet-Like token
+// https://github.com/fernet/spec/blob/0250c594a266036afb39aa574b88d9283810be47/Spec.md
+
 const crypto = require('crypto');
 
-const version = 'flv1';
+const version = '1';
 const separator = '-';
 const cryptoAlgorism = 'aes-128-cbc';
 const signerAlgorism = 'sha256';
 const encodeTo = 'base64';
 const decodeTo = 'utf-8';
 
-const encrypt = (payload, key) => {
-  const cipher = crypto.createCipher(cryptoAlgorism, key);
+const encrypt = (payload, key, iv) => {
+  const cipher = crypto.createCipheriv(cryptoAlgorism, key, iv);
   return cipher.update(payload, decodeTo, encodeTo) + cipher.final(encodeTo);
 };
 
-const decrypt = (crypted, key) => {
-  const decipher = crypto.createDecipher(cryptoAlgorism, key);
+const decrypt = (crypted, key, iv) => {
+  const decipher = crypto.createDecipheriv(cryptoAlgorism, key, iv);
   return decipher.update(crypted, encodeTo, decodeTo) + decipher.final(decodeTo);
 };
 
-const createHKDF = (key, salt) => {
-  const secureKey = crypto.createHmac(signerAlgorism, key)
-    .update(salt)
-    .digest(encodeTo)
-    .toString();
-  return payload => crypto
-    .createHmac(signerAlgorism, secureKey)
-    .update(payload)
-    .digest(encodeTo)
-    .toString();
-}
+const createHmac = key => payload => crypto
+  .createHmac(signerAlgorism, key)
+  .update(payload)
+  .digest(encodeTo)
+  .toString();
 
-const createFernetLike = ({ signerKey, cryptoKey, salt, getCurrentDate = () => new Date() }) => {
-  const hkdf = createHKDF(signerKey, salt);
+const createFernetLike = ({
+  signerKey,
+  cryptoKey,
+  randomBytes = () => crypto.randomBytes(16),
+  getCurrentDate = () => new Date(),
+}) => {
+  const hmac = createHmac(signerKey);
   return {
     sign(payload, maxAgeSec) {
-      const crypted = encrypt(payload, cryptoKey);
+      const iv = randomBytes();
+      const ivBase64 = iv.toString('base64');
+      const crypted = encrypt(payload, cryptoKey, iv);
       const expired = String(Math.floor(getCurrentDate().getTime() / 1000) + maxAgeSec);
-      const signature = hkdf(version + expired + crypted);
-      return [version, expired, crypted, signature].join(separator);
+      const signature = hmac(version + expired + ivBase64 + crypted);
+      return [version, expired, ivBase64, crypted, signature].join(separator);
     },
     verify(token) {
-      const [header, expired, crypted, signature] = token.split(separator);
+      const [header, expired, ivBase64, crypted, signature] = token.split(separator);
       if (Number(expired) * 1000 < getCurrentDate().getTime()) throw new Error('Expired token');
-      if (signature === hkdf(header + expired + crypted)) {
-        return decrypt(crypted, cryptoKey);
+      if (signature === hmac(header + expired + ivBase64 + crypted)) {
+        return decrypt(crypted, cryptoKey, Buffer.from(ivBase64, 'base64'));
       }
       throw new Error('Verification failure');
     },
--- modules/review-spa-cdn/lambda/fernetLike.test.js
+++ modules/review-spa-cdn/lambda/fernetLike.test.js
@@ -1,35 +1,35 @@
 const createFernetLike = require('./fernetLike');
 
 const signerKey = '16bytesaaaaaaaaa';
-const cryptoKey = 'decent-long-string-bbb';
-const salt = 'decent-long-string-ccc';
+const cryptoKey = '16bytesbbbbbbbbb';
+const iv = Buffer.from('16bytesccccccccc');
 
 test('sign', () => {
-  const { sign } = createFernetLike({ signerKey, cryptoKey, salt, getCurrentDate: () => new Date(1000) });
+  const { sign } = createFernetLike({ signerKey, cryptoKey, randomBytes: () => iv, getCurrentDate: () => new Date(1000) });
   const token = sign('I want to encrypt this string.', 60);
 
-  expect(token).toBe('flv1-61-rvrtDSwoKi0z8SB/2dTdUn38BUEl4mKFZVtpfGd9B3o=-M8/Ylt0x6Ukp2tn/aG7+j98q/C/HJOMuFlp7UqvGs34=');
+  expect(token).toBe('1-61-MTZieXRlc2NjY2NjY2NjYw==-d+7GOFT9EyCqnKQOQghiczXQAZY29RvdJyi+5gnmR3U=-E+7laPplIOhNh0sd7QwyvQ95LOr6hepvIlQPYS8yTSQ=');
 });
 
 test('verify ok', () => {
-  const { verify } = createFernetLike({ signerKey, cryptoKey, salt, getCurrentDate: () => new Date(1000) });
-  const payload = verify('flv1-61-rvrtDSwoKi0z8SB/2dTdUn38BUEl4mKFZVtpfGd9B3o=-M8/Ylt0x6Ukp2tn/aG7+j98q/C/HJOMuFlp7UqvGs34=');
+  const { verify } = createFernetLike({ signerKey, cryptoKey, randomBytes: () => iv, getCurrentDate: () => new Date(1000) });
+  const payload = verify('1-61-MTZieXRlc2NjY2NjY2NjYw==-d+7GOFT9EyCqnKQOQghiczXQAZY29RvdJyi+5gnmR3U=-E+7laPplIOhNh0sd7QwyvQ95LOr6hepvIlQPYS8yTSQ=');
 
   expect(payload).toBe('I want to encrypt this string.');
 });
 
 test('verify failure', () => {
-  const { verify } = createFernetLike({ signerKey, cryptoKey, salt, getCurrentDate: () => new Date(1000) });
+  const { verify } = createFernetLike({ signerKey, cryptoKey, randomBytes: () => iv, getCurrentDate: () => new Date(1000) });
 
   expect(
-    () => verify('flv1-777-rvrtDSwoKi0z8SB/2dTdUn38BUEl4mKFZVtpfGd9B3o=-M8/Ylt0x6Ukp2tn/aG7+j98q/C/HJOMuFlp7UqvGs34='),
+    () => verify('1-99-MTZieXRlc2NjY2NjY2NjYw==-d+7GOFT9EyCqnKQOQghiczXQAZY29RvdJyi+5gnmR3U=-E+7laPplIOhNh0sd7QwyvQ95LOr6hepvIlQPYS8yTSQ='),
   ).toThrow('Verification failure');
 });
 
 test('verify expired', () => {
-  const { verify } = createFernetLike({ signerKey, cryptoKey, salt, getCurrentDate: () => new Date(1000 + 61 * 1000) });
+  const { verify } = createFernetLike({ signerKey, cryptoKey, randomBytes: () => iv, getCurrentDate: () => new Date(1000 + 61 * 1000) });
 
   expect(
-    () => verify('flv1-61-rvrtDSwoKi0z8SB/2dTdUn38BUEl4mKFZVtpfGd9B3o=-M8/Ylt0x6Ukp2tn/aG7+j98q/C/HJOMuFlp7UqvGs34='),
+    () => verify('1-61-MTZieXRlc2NjY2NjY2NjYw==-d+7GOFT9EyCqnKQOQghiczXQAZY29RvdJyi+5gnmR3U=-E+7laPplIOhNh0sd7QwyvQ95LOr6hepvIlQPYS8yTSQ='),
   ).toThrow('Expired token');
 });
